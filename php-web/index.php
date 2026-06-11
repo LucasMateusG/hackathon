930e3bb7d6a84f192bbec84eb36e173ba37d76df
PedroGoncalvezSouza <pehenriquegdesouza@gmail.com>
2026-06-11 18:33:56 -0300

    estrutura do forms de login construidas para empresa e alunos

--- php-web/index.php
+++ php-web/index.php
@@ -6,28 +6,3 @@
 ?>
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <div>
-        <h1>Teste conexão com api via cUrl</h1>
-        <table>
-            <tr>
-                <th>Nome:</th>
-                <th>Email:</th>
-            </tr>
-            <tr>
-            <?php foreach ($users as $row): ?>
-                <td><?= $linha['name']?></td>
-                <td>><?= $linha['email']?></td>
-            <?php endforeach; ?>    
-            </tr>
-        </table>
-    </div>
-</body>
-</html>
